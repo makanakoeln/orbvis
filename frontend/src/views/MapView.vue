a5ba3fcc9858cb14eb39ca505cd99fc2e8ba2ee2
rb <ronny.bruska@gmail.com>
2026-03-08 08:31:11 +0100

    Remove number input spinners in Map Settings modal

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -652,3 +652,13 @@ onUnmounted(() => {
   statesStore.disconnect()
 })
 </script>
+
+<style scoped>
+input[type="number"] {
+  appearance: textfield;
+}
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+</style>
