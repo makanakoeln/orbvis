3b43a6922ea6989d4a934da33c1f1031fbfc7b80
rb <ronny.bruska@gmail.com>
2026-03-14 14:16:28 +0100

    home: show dynamic for readonly boards (demo boards)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -223,7 +223,7 @@
               </svg>
               <span class="font-mono truncate">{{ map.backend_id }}</span>
               <span class="text-zinc-600">·</span>
-              <span v-if="['automap', 'radar', 'worldmap'].includes(map.view.type)" class="italic">{{ t('home.dynamicObjects') }}</span>
+              <span v-if="map.readonly || ['automap', 'radar', 'worldmap'].includes(map.view.type)" class="italic">{{ t('home.dynamicObjects') }}</span>
               <span v-else>{{ t('common.objects', { n: map.object_count }) }}</span>
             </div>
           </div>
