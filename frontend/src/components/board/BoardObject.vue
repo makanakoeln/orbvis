fdeb5982e415e9882ca2bc324d75e02881bc2c8c
rb <ronny.bruska@gmail.com>
2026-03-21 12:11:59 +0100

    board: suppress arc ring for image objects when image fails to load

--- frontend/src/components/board/BoardObject.vue
+++ frontend/src/components/board/BoardObject.vue
@@ -185,7 +185,8 @@ const isSvgIcon = computed(() => {
 
 const shouldShowRing = computed(() =>
   !['textbox', 'line'].includes(props.object.type) &&
-  props.object.display?.mode !== 'gadget',
+  props.object.display?.mode !== 'gadget' &&
+  !(props.object.type === 'image' && imgLoadFailed.value),
 )
 
 useArcRing({
