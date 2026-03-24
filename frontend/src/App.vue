0a3cfdadbf79549ecd9220358113414889c11047
rb <ronny.bruska@gmail.com>
2026-03-24 21:19:22 +0100

    version: show changelog popup on first login after version update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,16 +4,18 @@
     <div class="flex-1 min-w-0 flex flex-col overflow-hidden">
       <router-view />
     </div>
+    <ChangelogModal v-if="showChangelog" @close="dismissChangelog" />
   </div>
   <router-view v-else />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, watch, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useTheme } from '@/composables/useTheme'
 import { useAuthStore } from '@/stores/auth'
 import AppSidebar from '@/components/AppSidebar.vue'
+import ChangelogModal from '@/components/ChangelogModal.vue'
 
 useTheme()
 const route = useRoute()
@@ -22,4 +24,18 @@ const auth = useAuthStore()
 const showShell = computed(() =>
   auth.user !== null && !['login', 'change-password'].includes(route.name as string)
 )
+
+const LS_KEY = 'orbvis_changelog_seen'
+const showChangelog = ref(false)
+
+watch(showShell, (val) => {
+  if (val && localStorage.getItem(LS_KEY) !== __APP_VERSION__) {
+    showChangelog.value = true
+  }
+}, { immediate: true })
+
+function dismissChangelog() {
+  localStorage.setItem(LS_KEY, __APP_VERSION__)
+  showChangelog.value = false
+}
 </script>
