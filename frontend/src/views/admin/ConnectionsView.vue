7c610910f31b5ff6a1e8f32eb3d230ee340c9d2c
rb <ronny.bruska@gmail.com>
2026-05-14 23:02:14 +0200

    connections: replace inline SVG icons with CmkIcon (add, edit, remove)

--- frontend/src/views/admin/ConnectionsView.vue
+++ frontend/src/views/admin/ConnectionsView.vue
@@ -10,19 +10,7 @@
                 </CmkParagraph>
             </div>
             <CmkButton variant="primary" @click="openCreate">
-                <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2.5"
-                    style="width: 13px; height: 13px; margin-right: var(--dimension-3)"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                </svg>
+                <CmkIcon name="add" size="small" style="margin-right: var(--dimension-3)" />
                 {{ t('admin.addConnection') }}
             </CmkButton>
         </div>
@@ -193,38 +181,14 @@
                                     :title="t('common.edit')"
                                     @click="openEdit(b)"
                                 >
-                                    <svg
-                                        style="width: 13px; height: 13px"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        stroke-width="2"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125"
-                                        />
-                                    </svg>
+                                    <CmkIcon name="edit" size="small" />
                                 </button>
                                 <button
                                     class="p-[4px] rounded-md text-[var(--text-muted)] hover:text-[var(--color-light-red-40)] hover:bg-[var(--color-light-red-50)]/10 transition-all"
                                     :title="t('common.delete')"
                                     @click="deleteTarget = b.id"
                                 >
-                                    <svg
-                                        style="width: 13px; height: 13px"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        stroke-width="2"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                        />
-                                    </svg>
+                                    <CmkIcon name="remove" size="small" />
                                 </button>
                             </div>
                         </td>
@@ -322,6 +286,7 @@
 import CmkAlertBox from '@cmk/components/CmkAlertBox.vue';
 import CmkBadge from '@cmk/components/CmkBadge.vue';
 import CmkButton from '@cmk/components/CmkButton.vue';
+import CmkIcon from '@cmk/components/CmkIcon';
 import CmkLabel from '@cmk/components/CmkLabel.vue';
 import CmkLoading from '@cmk/components/CmkLoading.vue';
 import CmkSlideInDialog from '@cmk/components/CmkSlideInDialog.vue';
